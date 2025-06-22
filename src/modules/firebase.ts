06142e7d4f730a4e3bf925e6ce42c52d9641b86d
YichuanZhangthemostHANDSOMEone <Yichuan.Zhang@my.jcu.edu.au>
2025-06-22 16:48:25 +0800

    updated firebase config

--- src/modules/firebase.ts
+++ src/modules/firebase.ts
@@ -1,16 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: 'YOUR_FIREBASE_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID'
+  apiKey: 'AIzaSyAHwfnuP5J8uVATBUxnwVhPnnbvDkP1gI4',
+  authDomain: 'internet-fundamental-guider.firebaseapp.com',
+  projectId: 'internet-fundamental-guider',
+  storageBucket: 'internet-fundamental-guider.firebasestorage.app',
+  messagingSenderId: '1053498918039',
+  appId: '1:1053498918039:web:54f2d56dc345f0a1b995d2',
+  measurementId: 'G-BKF4Y7CNS9'
 };
 
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
