acf84438e2892c74c979b27142905e61faffd8a0
YichuanZhangthemostHANDSOMEone <Yichuan.Zhang@my.jcu.edu.au>
2025-07-19 14:45:06 +0800

    fix: include OpenCV namespace types

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -4,7 +4,10 @@ declare module 'firebase/app';
 declare module 'firebase/auth';
 declare module 'firebase/firestore';
 declare module 'firebase/analytics';
-declare module 'quantize';
+declare module 'quantize' {
+  const quantize: any;
+  export default quantize;
+}
 declare module 'color.js';
 declare module 'colorjs.io';
 declare module 'colorjs.io/src/deltaE.js';
@@ -12,12 +15,10 @@ declare module '@techstark/opencv-js' {
   const cv: any;
   export default cv;
 }
-declare global {
-  namespace cv {
-    // 以后在签名里写 cv.Mat，TS 就认这是 any
-    type Mat       = any;
-    type MatVector = any;
-  }
+declare namespace cv {
+  // 以后在签名里写 cv.Mat，TS 就认这是 any
+  type Mat       = any;
+  type MatVector = any;
 }
 
 
